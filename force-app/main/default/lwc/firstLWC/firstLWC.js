8a4066fa84b797f3cf23093aa9893e4041d48440
Rajesh Duraisamy <ambikula@gmail.com>
2021-04-24 15:03:50 -0400

    iterator loop

---
+++ force-app/main/default/lwc/firstLWC/firstLWC.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class FirstLWC extends LightningElement {
+    comments = [
+        {name:'Ambika', message:'Hi How are you?'},
+        {name:'Rajesh', message:'Where are you?'},
+    ];
+}
